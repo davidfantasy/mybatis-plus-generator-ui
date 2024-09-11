ad280006c656c822c62231920ba627203482e19f
klaokai <573984425@qq.com>
2024-09-11 15:55:59 +0800

    chore(整理代码)

--- mybatis-plus-generator-ui-starter-webmvc-api/src/main/java/com/github/davidfantasy/mybatisplus/generatorui/webmvc/api/core/SpringDocWebMvcConfiguration.java
+++ mybatis-plus-generator-ui-starter-webmvc-api/src/main/java/com/github/davidfantasy/mybatisplus/generatorui/webmvc/api/core/SpringDocWebMvcConfiguration.java
@@ -1,11 +1,13 @@
 package com.github.davidfantasy.mybatisplus.generatorui.webmvc.api.core;
 
 import com.github.davidfantasy.mybatisplus.generatorui.common.core.configuration.MybatisplusGeneratoruiAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.*;
-import org.springframework.context.annotation.Bean;
+import com.github.davidfantasy.mybatisplus.generatorui.webmvc.api.api.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.web.servlet.function.RouterFunction;
 
 import static com.github.davidfantasy.mybatisplus.generatorui.common.util.Constants.MYBATISPLUS_GENETATORUI_ENABLED;
 
@@ -19,29 +21,12 @@ import static com.github.davidfantasy.mybatisplus.generatorui.common.util.Consta
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @ConditionalOnProperty(name = MYBATISPLUS_GENETATORUI_ENABLED, matchIfMissing = true)
 @ConditionalOnBean(MybatisplusGeneratoruiAutoConfiguration.class)
+@Import({AutoCompleteController.class,
+        DatabaseController.class,
+        MbpGeneratorController.class,
+        OpenApiWebMvcResource.class,
+        OutputFileInfoController.class,
+        SqlGeneratorController.class,
+        TemplateController.class})
 public class SpringDocWebMvcConfiguration {
-
-    static {
-    }
-
-    /**
-     * The type Spring doc web mvc router configuration.
-     *
-     * @author bnasslahsen
-     */
-    @ConditionalOnClass(RouterFunction.class)
-    static class SpringDocWebMvcRouterConfiguration {
-
-        /**
-         * Router function provider router function provider.
-         *
-         * @return the router function provider
-         */
-        @Bean
-        @ConditionalOnMissingBean
-        @Lazy(false)
-        RouterFunctionWebMvcProvider routerFunctionProvider() {
-            return new RouterFunctionWebMvcProvider();
-        }
-    }
 }
